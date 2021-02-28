3f36ac87551ba42ed63d791d49ff9a1d76dd9478
Patryk Plaza <atrykp0@gmail.com>
2021-02-28 06:36:00 +0100

    change the order of the cards

--- js/Box.js
+++ js/Box.js
@@ -1,31 +1,26 @@
-import {
-    Common
-} from "./Common.js";
+import { Common } from "./Common.js";
 
-const BOX_NEW_CARDS_CLASS_NAME = '.newCards';
-const BOX_CAN_CARDS_CLASS_NAME = '.canCards';
-const COUNTER_SPAN_CLASS = '.counter';
+const BOX_NEW_CARDS_CLASS_NAME = ".newCards";
+const BOX_CAN_CARDS_CLASS_NAME = ".canCards";
+const COUNTER_SPAN_CLASS = ".counter";
 export class Box extends Common {
-    constructor() {
-        super()
-        this.newCards = this.bindToHtmlElement(BOX_NEW_CARDS_CLASS_NAME);
-        this.canCards = this.bindToHtmlElement(BOX_CAN_CARDS_CLASS_NAME);
-        this.counter = 0;
-        this.counterSpan = this.bindToHtmlElement(COUNTER_SPAN_CLASS);
-    }
-    addCardToBox(card) {
-        this.newCards.appendChild(card)
-    }
-    moveToCan(card) {
-        this.canCards.appendChild(card);
-    }
-    updateCouter() {
-
-        this.counter = this.canCards.childElementCount;
-        this.counterSpan.innerHTML = this.canCards.childElementCount
-    }
-
+  constructor() {
+    super();
+    this.newCards = this.bindToHtmlElement(BOX_NEW_CARDS_CLASS_NAME);
+    this.canCards = this.bindToHtmlElement(BOX_CAN_CARDS_CLASS_NAME);
+    this.counter = 0;
+    this.counterSpan = this.bindToHtmlElement(COUNTER_SPAN_CLASS);
+  }
+  addCardToBox(card) {
+    this.newCards.prepend(card);
+  }
+  moveToCan(card) {
+    this.canCards.prepend(card);
+  }
+  updateCouter() {
+    this.counter = this.canCards.childElementCount;
+    this.counterSpan.innerHTML = this.canCards.childElementCount;
+  }
 }
 
-
-export const box = new Box()
+export const box = new Box();
